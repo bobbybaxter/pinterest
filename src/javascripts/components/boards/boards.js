42f02a9e13b4cb1c187e2047563d134af0fe3410
Bob <bob.e.baxter@gmail.com>
2019-05-14 19:24:45 -0500

    boards are printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,30 @@
+import $ from 'jquery';
 import boardData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+const boardBuilder = (boards) => {
+  let domString = '';
+  $.each(boards, (i) => {
+    // card background
+    domString += '<div class="card card-image bg-secondary col-3 board-card">';
+    // card
+    domString += '<div class="text-white text-center d-flex align-items-center rgba-stylish-strong">';
+    // card content
+    domString += '<div class="card-body">';
+    domString += `<h5>${boards[i].name}</h5>`;
+    domString += '<a class="btn btn-outline-light"># of pins</a>';
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('response', resp.data.boards);
+      boardBuilder(resp.data.boards);
+      // console.error('response', resp.data.boards);
     })
     .catch(err => console.error('error from loadBoards', err));
 };
