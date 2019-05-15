3f809757dd006100cf967b4bd6697feabece1509
Bob <bob.e.baxter@gmail.com>
2019-05-14 20:45:38 -0500

    learned to make promises i can keep

---
+++ src/javascripts/helpers/data/pinsData.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const loadPinsFromBoard = boardId => new Promise((resolve, reject) => {
+  axios.get('../db/pins.json')
+    .then((resp) => {
+      const allPins = resp.data.pins;
+      // filter the pins
+      console.error('boardId inside pins', boardId);
+      const matchingPins = allPins.filter(pin => pin.boardId === boardId);
+      // resolve the filtered pins
+      resolve(matchingPins);
+    })
+    .catch(err => reject(err));
+});
+
+export default { loadPinsFromBoard };
